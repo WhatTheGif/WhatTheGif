e88942829e16aea32b75bee1c204203174b37941
Jehovany Cruz <jehovany@Jehovanys-Air.fios-router.home>
2020-08-11 19:52:23 -0400

    updated maingame logic, judge logic, websocket for judge, judge id vs player id

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useRef, useContext } from 'react';
+import SocketContext from '../context/SocketContext';
+import { Route, Switch, Link } from 'react-router-dom';
+import Splash from './Splash';
+import MainGame from './MainGame';
+import EndGame from './EndGame';
 
 const App = () => {
-  return <h1>Hello World</h1>;
-};
+  const [gameOn, setGameOn] = useState(false);
+  const [gameEnd, setGameEnd] = useState(false);
+
+  const socket = useContext(SocketContext);
+
+  socket.on('redirect', function (destination) {
+    // console.log('we in here', destination);
+    // window.location.href = destination;
+    if (destination === '/game') {
+      setGameOn(true);
+    }
+  });
+
+  if (gameOn) {
+    return <MainGame />;
+  }
+  if (gameEnd) {
+    return <EndGame />;
+  }
+  return <Splash />;
+}
 
 export default App;

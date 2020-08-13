b661a83c356bc9b66afd712089ce2d0a546e9f48
howaboutjeho <jac783@nyu.edu>
2020-08-13 12:45:46 -0400

    fixed random gif problem and added logo to oauth

--- client/components/GoogleOAuth.js
+++ client/components/GoogleOAuth.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import SplashPage from './Splash';
+import React, { useState } from "react";
+import SplashPage from "./Splash";
 const { CLIENT_ID } = process.env;
 
 const GoogleOAuth = () => {
   const [google, setgoogle] = useState(false);
   function initializeGoogleSignIn() {
-    window.gapi.load('auth2', () => {
+    window.gapi.load("auth2", () => {
       window.gapi.auth2
         .init({
           client_id: CLIENT_ID,
@@ -26,11 +26,20 @@ const GoogleOAuth = () => {
   if (google.isSignedIn === true) {
     currentView = <SplashPage />;
   } else {
-    const script = document.createElement('script');
-    script.src = 'https://apis.google.com/js/platform.js';
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/platform.js";
     script.onload = () => initializeGoogleSignIn();
     document.body.appendChild(script);
-    currentView = <div className="g-signin2" data-onsuccess="onSignIn"></div>;
+    currentView = (
+      <div className="oauth">
+        <img
+          alt="logo"
+          src="https://fontmeme.com/permalink/200812/494bb6ee41bd84762de732c77da6bd2f.png"
+          width="100%"
+        />
+        <div className="g-signin2" data-onsuccess="onSignIn"></div>
+      </div>
+    );
   }
   return <div>{currentView}</div>;
 };
